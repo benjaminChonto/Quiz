f2f9461919ac8b5a8aaa88adf7d180cd5ffcef25
benjaminChonto <100520250+benjaminChonto@users.noreply.github.com>
2022-06-16 15:27:02 +0200

    Add files via upload

--- feladat01.java
+++ feladat01.java
@@ -4,10 +4,10 @@ public class feladat01 {
         if(colors.length == 0) return 0;
         int subsetCount = 1;
         for(int i = 0; i< colors.length; ++i){
-            subsetCount += 2*i;
+            subsetCount *= 2;
         }
         //Returning the number of subsets with more than one element;
-        return subsetCount - colors.length;
+        return subsetCount - colors.length - 1;
     }
 
     public static void main(String[] args){
